92009502abc466cd8141c3cb1398bccfba893781
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-17 19:54:03 -0500

    solucion del problema

--- sumaDinero3Personas/src/sumadinero3personas/SumaDinero3Personas.java
+++ sumaDinero3Personas/src/sumadinero3personas/SumaDinero3Personas.java
@@ -1,10 +1,22 @@
 
 package sumadinero3personas;
 
+import java.util.Scanner;
+
 public class SumaDinero3Personas {
 
     public static void main(String[] args) {
         
+        Scanner entrada = new Scanner(System.in);
+        
+        float guillermo, luis, juan;
+        
+        System.out.print("Ingresa el dinero de Guillermo: ");
+        guillermo = entrada.nextFloat();
+        luis = guillermo/2;
+        juan = (guillermo + luis)/2;
+        
+        System.out.println("Entre los tres tienen un total de:" + "$" + (guillermo + luis + juan));
     }
     
 }
